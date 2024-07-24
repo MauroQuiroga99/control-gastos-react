774bea4cc398bfbdf92ff3cfc5af8a05e89f51ff
Mauro Quiroga <148659366+MauroQuiroga99@users.noreply.github.com>
2024-07-24 18:09:07 -0500

    crear formulario para definir el presupuesto

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,5 +1,34 @@
-import React from "react";
+import { useState } from "react";
 
-export default function GetForm() {
-  return <div></div>;
+export default function BudgetForm() {
+  const [budget, setBudget] = useState(0);
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setBudget(e.target.valueAsNumber);
+  };
+  return (
+    <form className="space-y-5">
+      <div className="flex flex-col ">
+        <label
+          htmlFor="budget"
+          className=" mb-4 text-4xl text- lue-600 font-bold text-center"
+        >
+          Definir presupuesto
+        </label>
+        <input
+          id="budgetID"
+          type="number"
+          className="w-full bg-white border border-gray-200 rounded-lg p-2"
+          placeholder="Define tu presupuesto"
+          name="budget"
+          onChange={handleChange}
+        />
+      </div>
+
+      <input
+        type="submit"
+        placeholder="Definir Presupuesto"
+        className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-black uppercase"
+      />
+    </form>
+  );
 }
