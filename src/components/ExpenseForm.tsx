756e91a99a78aa0a66c3d48db47bbc8db79935db
Mauro Quiroga <148659366+MauroQuiroga99@users.noreply.github.com>
2024-07-27 16:48:56 -0500

    crear el formulario de gastos

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,7 +1,54 @@
+import { categories } from "../data/categories";
+
 export default function ExpenseForm() {
   return (
-    <div>
-      <p>Hola</p>
-    </div>
+    <form className="space-y-5" action="">
+      <legend className=" uppercase text-center text-2xl font-black border-b-4 py-2 border-blue-500">
+        Nuevo gasto
+      </legend>
+      <div className="flex flex-col gap-2">
+        <label htmlFor="expenseName" className="text-xl">
+          Nombre de gasto:
+        </label>
+        <input
+          type="text"
+          id="expenseName"
+          placeholder="Añade el nombre del gasto"
+          className="bg-slate-100 p-2 "
+          name="expenseName"
+        />
+      </div>
+      <div className="flex flex-col gap-2">
+        <label htmlFor="amount" className="text-xl">
+          Cantidad:
+        </label>
+        <input
+          type="number"
+          id="amount"
+          placeholder="Añade la cantidad del gasto: ej. 300"
+          className="bg-slate-100 p-2 "
+          name="amount"
+        />
+      </div>
+      <div className="flex flex-col gap-2">
+        <label htmlFor="categorie" className="text-xl">
+          Categoría:
+        </label>
+        <select id="categorie" className="bg-slate-100 p-2 " name="categorie">
+          <option value="">--Seleccione--</option>
+          {categories.map((category) => (
+            <option value={category.id} key={category.id}>
+              {" "}
+              {category.name}{" "}
+            </option>
+          ))}
+        </select>
+        <input
+          type="submit"
+          className="bg-blue-600 cursor-pointer w-full p-2 text-white uppercase font-bold rounded-lg"
+          value={"Registrar Gasto "}
+        />
+      </div>
+    </form>
   );
 }
