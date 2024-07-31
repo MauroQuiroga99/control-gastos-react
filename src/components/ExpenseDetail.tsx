64e044937b098d787495d0d354e79d8a84adb906
Mauro Quiroga <148659366+MauroQuiroga99@users.noreply.github.com>
2024-07-31 09:29:37 -0500

    mostrar gastos

--- src/components/ExpenseDetail.tsx
+++ src/components/ExpenseDetail.tsx
@@ -1,9 +1,24 @@
+import { formatDate } from "../helpers";
 import { Expense } from "../types";
+import AmountDisplay from "./AmountDisplay";
 
 type ExpenseDetailProps = {
   expense: Expense;
 };
 
 export default function ExpenseDetail({ expense }: ExpenseDetailProps) {
-  return <div className="bg-red-600">Hola Joven</div>;
+  return (
+    <div className="bg-white shadow-lg p-10 w-full border-b border-gray-200 gap-4 flex items-center">
+      <div></div>
+      <div>
+        <p> {expense.expenseName} </p>
+        <p className="text-slate-600 text-sm">
+          {" "}
+          {formatDate(expense.date!.toString())}{" "}
+        </p>
+      </div>
+
+      <AmountDisplay label={""} amount={expense.amount} />
+    </div>
+  );
 }
