3ac290c5522371cee40f52be94a657f5cbe48a23
Mauro Quiroga <148659366+MauroQuiroga99@users.noreply.github.com>
2024-08-06 16:14:25 -0500

    eliminar gasto al ejecutar swipe

--- src/components/ExpenseDetail.tsx
+++ src/components/ExpenseDetail.tsx
@@ -11,12 +11,14 @@ import { Expense } from "../types";
 import AmountDisplay from "./AmountDisplay";
 import { categories } from "../data/categories";
 import "react-swipeable-list/dist/styles.css";
+import { useBudget } from "../hooks/useBudget";
 
 type ExpenseDetailProps = {
   expense: Expense;
 };
 
 export default function ExpenseDetail({ expense }: ExpenseDetailProps) {
+  const { dispatch } = useBudget();
   const categoryInfo = useMemo(
     () => categories.filter((cat) => cat.id === expense.category)[0],
     [expense]
@@ -30,7 +32,12 @@ export default function ExpenseDetail({ expense }: ExpenseDetailProps) {
 
   const trailingActions = () => (
     <TrailingActions>
-      <SwipeAction onClick={() => {}} destructive={true}>
+      <SwipeAction
+        onClick={() =>
+          dispatch({ type: "remove-expense", payload: { id: expense.id } })
+        }
+        destructive={true}
+      >
         Eliminar
       </SwipeAction>
     </TrailingActions>
@@ -38,7 +45,7 @@ export default function ExpenseDetail({ expense }: ExpenseDetailProps) {
   return (
     <SwipeableList>
       <SwipeableListItem
-        maxSwipe={30}
+        maxSwipe={1}
         leadingActions={leadingActions()}
         trailingActions={trailingActions()}
       >
